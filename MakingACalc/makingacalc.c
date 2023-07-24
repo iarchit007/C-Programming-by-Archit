686cea37e0457df3f7ddda05523b8fe23793f0df
iarchit007 <architchoudaha@gmail.com>
2023-07-24 12:57:41 +0530

    Making a calculator (day 3)
    Added if-if ladder and else statement. Corrected some minor typing mistakes.

--- MakingACalc/makingacalc.c
+++ MakingACalc/makingacalc.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 main()
 {
-    float a,b,c,d,e,f,g;
+    float a,b,c,d,e,f;
+    int i1, i2;
     printf("Enter A Number (a): ");
     scanf("%f",&a);
     printf("Enter another Number (b): ");
-    scanf("%d",&a);
-    c=a+b; d=a-b; e=a*b; f=a/b;
-    printf("a+b= %f",c);
-    prinitf("a-b= %f",d);
-    printf("a*b= %f",e);
-    prinitf("a/b= %f",f);
+    scanf("%f",&b);
+    printf("Which operation do you want to perform? \n1. a+b\n2. a-b\n3. a*b\n4. a/b\nEnter number which is given before the operator to proceed with the desired operation:  ");
+    scanf("%d",&i1);
+    if(i1==1)
+    {
+        
+        printf("a+b= %f",a+b);
+    }
+    if(i1==2)   
+    {
+        
+        printf("a-b= %f",a-b);
+    }
+    if(i1==3)
+    {
+        
+        printf("a*b= %f",a*b);
+    }
+    if(i1==4)
+    {
+        
+        printf("a/b= %f",a/b);
+    }
+    else
+    {
+        printf("Invalid");
+    }
 }
