5d2cad6404c859c7f243bf0975fdf2ab2ed1ed9a
iarchit007 <architchoudaha@gmail.com>
2023-07-24 12:40:22 +0530

    Making of a calculator  (day 1)
    Basic calculator using integer data type (%d).

---
+++ MakingACalc/makingacalc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+    int a,b,c;
+    printf("Enter A Number (a): ");
+    scanf("%d",&a);
+    printf("Enter another Number (b): ");
+    scanf("%d",&a);
+    printf("a+b= %d",a+b);
+    prinitf("a-b= %d",a-b);
+    printf("a*b= %d",a*b);
+    prinitf("a/b= %d",a/b);
+}
