a9fa2eae5b8c4ac256ddaccd404d78c03abc57b6
iarchit007 <architchoudaha@gmail.com>
2023-07-24 13:03:58 +0530

    Making a calculator (day 5)
    Changing else-if ladder to switch case. Which will make the program much more faster (but lengthy).

--- MakingACalc/makingacalc.c
+++ MakingACalc/makingacalc.c
@@ -9,28 +9,35 @@ main()
     scanf("%f",&b);
     printf("Which operation do you want to perform? \n1. a+b\n2. a-b\n3. a*b\n4. a/b\nEnter number which is given before the operator to proceed with the desired operation:  ");
     scanf("%d",&i1);
-    if(i1==1)
+    switch(i1)
+    {
+    case 1:
     {
         
         printf("a+b= %f",a+b);
     }
-    else if(i1==2)   
+    break;
+    case 2:   
     {
         
         printf("a-b= %f",a-b);
     }
-    else if(i1==3)
+    break;
+    case 3:
     {
         
         printf("a*b= %f",a*b);
     }
-    else if(i1==4)
+    break;
+    case 4:
     {
         
         printf("a/b= %f",a/b);
     }
-    else
+    break;
+    default:
     {
         printf("Invalid");
+    }
     }
 }
